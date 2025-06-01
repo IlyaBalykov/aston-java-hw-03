61b4a2982e41a50816bd1f14221f63ebbe5ce8b2
Ilya Balykov <balykov.ilya.88@gmail.com>
2025-06-01 22:12:48 +0300

    feat: add Throwable cause argument in constructor

--- src/FileOperationException.java
+++ src/FileOperationException.java
@@ -1,13 +1,13 @@
 public class FileOperationException extends Exception{
     private int code;
 
-    public FileOperationException(String message) {
-        super(message);
-    }
-
-    public FileOperationException (String message, int code) {
+    public FileOperationException(String message, int code) {
         super(message);
         this.code = code;
+    }
+
+    public FileOperationException (String message, Throwable cause) {
+        super(message, cause);
     }
 
     public int getCode() {
